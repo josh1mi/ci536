7017e715b490617fb1b6275365793fdede77b30a
Ben Highgate <91388545+Mageconvict@users.noreply.github.com>
2023-05-06 14:28:46 +0100

    ChatterBot
    Added logic adapters and training data

--- AI Python/PyTorchBackend.py
+++ AI Python/PyTorchBackend.py
@@ -1,6 +1,29 @@
 from chatterbot import ChatBot
 
-bot = ChatBot('Bicker')
+bot = ChatBot(
+    'Bicker',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+
+from chatterbot.trainers import ListTrainer
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+    'How are you?',
+    'I am good.',
+    'That is good to hear.',
+    'Thank you.',
+    'You are welcome.'
+])
+
+print('Type something to begin...')
 
 while True:
     try:
